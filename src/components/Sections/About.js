8a09e8c020bc6ae44f38c4ab69606b0b3884cd50
EllizWhilliane <ellizwhilliane.ewod@gmail.com>
2024-06-06 21:38:16 -0300

    teste

--- src/components/Sections/About.js
+++ src/components/Sections/About.js
@@ -7,6 +7,7 @@ export function About() {
       <div className="text-container">
         <h2 className="subtitle">O jeito mais prático e rápido de gerir agendamentos de clínicas e hospitais veterinarios</h2>
         <h2 className="subtitle">Otimize o agendamento de consultas veterinárias em sua clínica ou hospital com o PetMeets</h2>
+        <h2>testes de mensagem</h2>
         <p></p>
         <button className="button">Saiba Mais</button>
       </div>
