137d760c322e87e3026b5364536b685f3a71d6f9
EllizWhilliane <ellizwhilliane.ewod@gmail.com>
2024-06-06 21:23:28 -0300

    Add Navbar, criando um componente e configurando sass.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Navbar } from './components/Navbar'; // Importação do Navbar
 import { Home } from './pages/Home';
 import "./styles/main.sass";
 
 function App() {
   return (
     <Router>
+      <Navbar /> {/* Renderização do Navbar */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../../styles/components/_navbar.sass';
+import logo from '../../img/logo_petmeets.png'; // Ajuste o caminho conforme necessário
+
+export function Navbar() {
+  return (
+    <header className="navbar">
+      <nav className="navbar-container ">
+        <img
+          alt="Logo PetMeets"
+          loading="lazy"
+          width="220"
+          height="48"
+          decoding="async"
+          src={logo}
+          className="navbar-logo"
+        />
+        <ul className="navbar-links">
+          <li><a href="#home"><span>Home</span></a></li>
+          <li><a href="#partners"><span>Parceiros</span></a></li>
+          <li><a href="#blog"><span>Blog</span></a></li>
+        </ul>
+        <div className="navbar-buttons">
+          <a target="_blank" href="https://app.tec.pet/#/auth/login">
+            <button className="navbar-button">Login</button>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
